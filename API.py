85ce5f832144e87b007aaac170319be922733056
M1ch3I <michel@tie-International.com>
2022-07-07 19:51:38 +0200

    optimized pdf and implemented send email

--- API.py
+++ API.py
@@ -1,43 +1,85 @@
-from symtable import Symbol
-from requests import Request, Session
+import requests
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
-import json
-from Keys import CoinMarketCapKey
 import datetime
-from datetime import date, timedelta
+from datetime import timedelta
+from fpdf import FPDF
+import os
+from dotenv import load_dotenv
+import base64
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import (
+    Mail, Attachment, FileContent, FileName, FileType, Disposition, ContentId)
 
-url = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+load_dotenv()
+
+CoinMarketCapKey = os.getenv("COIN_MARKET_CAP_KEY")
+SendGridKey = os.getenv("SEND_GRID_KEY")
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+
 parameters = {
     'start': '1',
     'limit': '5000',
     'convert': 'USD'
 }
+
 headers = {
-    'Accepts': 'application/json',
-    'X-CMC_PRO_API_KEY': CoinMarketCapKey,
+    'X-CMC_PRO_API_KEY': CoinMarketCapKey
 }
 
-session = Session()
-session.headers.update(headers)
-
 try:
-    response = session.get(url, params=parameters)
-    data = json.loads(response.text)
 
-    today = date.today()
+    today = datetime.datetime.now()
+
     yesterday = str(today - timedelta(days=1))
-    yesterday_datetime = datetime.datetime.strptime(yesterday, '%Y-%m-%d')
+    yesterday_datetime = yesterday
+
+    currencies = []
+
+    r = requests.get(url, params=parameters, headers=headers)
+    data = r.json()
 
     for entry in data["data"]:
         symbol = entry["symbol"]
+        date_added_str = entry["date_added"].replace(
+            "T", " ").replace("Z", " ")
 
-        date_added_str = entry["date_added"][:10]
-        date_added = datetime.datetime.strptime(date_added_str, '%Y-%m-%d')
-
-        if yesterday_datetime < date_added:
-            print(symbol + ": " + date_added_str)
+        if yesterday_datetime < date_added_str:
+            currencies.append(symbol + ": " + date_added_str)
         else:
             pass
 
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Arial", size=15)
+    pdf.cell(200, 10, txt="New Cryptocurrencies on CoinMarketCap", ln=1, align='C')
+    for text in currencies:
+        pdf.cell(200, 10, txt=(text),
+                 ln=20, align='C')
+    pdf.output("NewCurrencies.pdf")
+
 except (ConnectionError, Timeout, TooManyRedirects) as e:
     print(e)
+
+message = Mail(
+    from_email='tbz.testmail@gmail.com',
+    to_emails='michel.studer@edu.tbz.ch',
+    subject='New Crypto currrencies in the last 24 hours',
+    html_content='<strong>See attachment for infos.</strong>'
+)
+file_path = 'NewCurrencies.pdf'
+with open(file_path, 'rb') as f:
+    data = f.read()
+encoded = base64.b64encode(data).decode()
+attachment = Attachment()
+attachment.file_content = FileContent(encoded)
+attachment.file_type = FileType('application/pdf')
+attachment.file_name = FileName('NewCurrencies.pdf')
+attachment.disposition = Disposition('attachment')
+attachment.content_id = ContentId('PDF File')
+message.attachment = attachment
+try:
+    sendgrid_client = SendGridAPIClient(SendGridKey)
+    response = sendgrid_client.send(message)
+except Exception as e:
+    print(e.message)
--- Keys.py
+++
@@ -1,1 +0,0 @@
-CoinMarketCapKey = "9216a41c-b680-4a6d-bf85-953ad3375a0c"
